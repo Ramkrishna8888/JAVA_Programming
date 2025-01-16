5e7dbac1504e1127481d140184c714c85e4947d8
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-01-16 17:00:51 +0530

    Update GreatestofThree.java

--- Java basics/GreatestofThree.java
+++ Java basics/GreatestofThree.java
@@ -1,3 +1,40 @@
+//Basic approach
+import java.util.*;
+public class GreaterofThree {
+    public static void main(String[] args)
+    {
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter any three numbers");
+    	int a = sc.nextInt();
+    	int b = sc.nextInt();
+    	int c = sc.nextInt();
+    	int greater;
+    	if(a>b)
+    	{
+    		if(a>c)
+    		{
+    			greater = a;
+    		}
+    		else
+    		{
+    			greater = c;
+    		}
+    	}
+    	else if(b>c)
+    	{
+    		greater = b;
+    	}
+    	else
+    	{
+    		greater = c;
+    	}
+    	System.out.println("The greatest number is = "+greater);
+    }
+}
+
+
+// Using  Logical operator
+
 import java.util.*;
 public class GreaterofThree {
     public static void main(String[] args)
