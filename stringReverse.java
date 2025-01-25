b74844ded9905ef57d273225183d630072f18d5b
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-01-25 14:32:51 +0530

    Create stringReverse.java

---
+++ stringReverse.java
@@ -0,0 +1,18 @@
+// basic questions before diving into string tricky questions
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String num = sc.next();
+        int i = num.length()-1;
+        String reverse = "";
+        while(i>=0)
+        {
+           reverse=reverse+num.charAt(i);
+           i--;
+        }
+        System.out.println(reverse);
+        
+    }
+}
