e4cbf8eb4f436a7bbcfa123a4dddbac31c5d6745
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-01-28 12:34:09 +0530

    Create operation.java

---
+++ OOP/PACKAGES/operation.java
@@ -0,0 +1,16 @@
+// creation of package in java
+package operation;
+
+public class calculate {
+
+		public int add(int a,int b)
+		{
+			return a+b; 
+		}
+		
+		public int multiply(int a, int b)
+		{
+			return a*b;
+		}
+
+}
