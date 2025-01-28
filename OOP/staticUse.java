8819c4bb0fa29fc65a1c5bfa3dcf75bcc2698aff
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-01-28 11:31:33 +0530

    Create staticUse.java

---
+++ OOP/staticUse.java
@@ -0,0 +1,31 @@
+// Understanding the use of Static keyword in JAVA OOP
+class Myclass
+{
+	   int m;
+	   static int n;
+	   void temp()
+	   {
+		   m = 10;
+		   n = 20;
+	   }
+	   static void temp2() {
+		   n=50;
+	   }
+	   void display()
+	   {
+		   System.out.println("m = "+m+" n = "+n);
+	   }
+}
+public class static1 {
+    public static void main(String[] args) {
+    	System.out.println("calling normal method");
+    	Myclass obj1 = new Myclass();
+    	obj1.temp();
+    	obj1.display();
+    	System.out.println("After calling the static method with second object");
+    	Myclass obj2 = new Myclass();
+    	obj2.temp();
+    	obj2.temp2();
+    	obj2.display();
+    }
+   }
