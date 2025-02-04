648c07142438507ee7a8b8d8c91e8aed8815b31f
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-02-04 19:16:28 +0530

    Create Exception1.java

---
+++ Exception Handling/Exception1.java
@@ -0,0 +1,32 @@
+
+public class Exception1 {
+
+	public static void main(String[] args) {
+		try {
+			int n1 = Integer.parseInt(args[0]);
+			int n2 = Integer.parseInt(args[1]);
+			int res = n1/n2;
+			System.out.println("Result = "+res);
+		}
+		catch(ArithmeticException e) {
+			System.err.println("Divide by zero not allowed");
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.err.println("Requires two numbers");
+		}
+		catch(NumberFormatException e) {
+			System.err.println("Enter only numbers");
+		}
+		catch(RuntimeException e) {
+			System.err.println("Runtime exception occured");
+		}
+		catch(Exception e) {
+			System.out.println("Exception occured");
+		}
+
+	}
+	
+	// For running the command line argument based program 
+	// Right click -> Run Configuration -> Select Class -> arguments ->give arguments with comma seperated and Run
+
+}
