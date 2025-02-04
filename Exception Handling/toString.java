9e0e8a0915b3d4815b56e36965d044e000d8f0b2
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-02-04 22:32:01 +0530

    Create toString.java

---
+++ Exception Handling/toString.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Product
+{
+   int pid;
+   String pname;
+   double price;
+   Product(int pid, String pname, double price){
+	   this.pid = pid;
+	   this.pname = pname;
+	   this.price=price;
+   }
+   Product(){}; // when your parameterized constructor is created then it's a good practice to create a default contructor
+	   public String toString() { // if you will not create a toString method then you'll get class name and hashcode as output
+		   return pid+" "+pname+" "+price; // because it'll extend object class of java implicitly 
+	   }
+   }
+
+public class ToStringDemo {
+
+	public static void main(String[] args) {
+		Product obj = new Product(123, "Mouse", 200.0);
+		System.out.println(obj);
+
+	}
+
+}
