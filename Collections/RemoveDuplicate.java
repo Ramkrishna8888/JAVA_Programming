d34d416c28a453655aa6c94d2bbc85249fb64e9d
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-02-05 18:39:56 +0530

    Create RemoveDuplicate.java

---
+++ Collections/RemoveDuplicate.java
@@ -0,0 +1,26 @@
+// Write a program to remove duplicate mobile numbers
+
+import java.util.*;
+public class RemoveDuplicate {
+
+	public static void main(String[] args) {
+		  List<Integer> mobileno = new ArrayList<Integer>();  
+          mobileno.add(1111);
+          mobileno.add(2222);
+          mobileno.add(3333);
+          mobileno.add(1111);
+          mobileno.add(2222);
+          mobileno.add(4444);
+          mobileno.add(5555);
+          mobileno.add(5555);
+		  System.out.println(mobileno);//[1111, 2222, 3333, 1111, 2222, 4444, 5555, 5555]
+		 
+		  Set<Integer> uniquemobno = new LinkedHashSet<Integer>(); // used LinkedHashSet because i'm worried about the order
+		  //addAll is used to add one collection object to another collection object
+		  // while add is used to add the elements in a set or list
+		  uniquemobno.addAll(mobileno);
+		  System.out.println("After using the LinkedHashSet");
+		  System.out.println(uniquemobno);
+	}
+
+}
