13de037c94a7fb4b151f1fa413164e670e5e97d4
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-02-12 12:16:26 +0530

    Update AgeCategoryPractice.java

--- Regular expression/AgeCategoryPractice.java
+++ Regular expression/AgeCategoryPractice.java
@@ -1,57 +1,49 @@
-// don't know Why I've made this program haha..
-// But you're gonna learn a lot of things by this program
-
 import java.util.*;
-public class UserInterface
-{
-    void checkValid(String id){
-        
-        if(id.length()!=8){
-            System.out.println(id+" is an invalid length");
+
+public class Main {
+
+    void checkValid(String id) {
+        if (id.length() != 8) {
+            System.out.println(id.length() + " is an invalid length");
             return;
         }
-        
-        String regex1 = "^[A-Z]{3}[0-9]{5}$";
-        if(!id.matches(regex1))
-        {
-            
-          System.out.println(id+" is an invalid format");
+
+        String regex = "^[A-Z]{3}[0-9]{5}$";
+        if (!id.matches(regex)) {
+            System.out.println(id + " is an invalid format");
             return;
-            
         }
-        else
-        {
-              String takeFirstDig = id.substring(3);
-            int checkFirstDig = Integer.parseInt(takeFirstDig);
-            if(checkFirstDig==0)
-            {
-                System.out.println(checkFirstDig+" is an invalid digit");
-                 return;
-            }
-           
-          
-          String takeAge = id.substring(7);
-          int checkAge = Integer.parseInt(takeAge);
-          
-          if(checkAge==1){
-              System.out.println("Age group belongs to 20 - 30");
-          }
-          else if(checkAge==2){
-              System.out.println("Age group belongs to 30 - 40");
-          }
-          else{
-              System.out.println("Age is not eligible");
-          }
+
+        String digPart = id.substring(3);
+
+        if (digPart.charAt(0) == '0') {
+            System.out.println(digPart.charAt(0) + " is an invalid digit");
+            return;
         }
+
+    String lastDigitStr = digPart.substring(4);
+      int lastDigit = Integer.parseInt(lastDigitStr); 
+
+    if (lastDigit < 1 || lastDigit > 9) {
+    System.out.println("Age category is an invalid category");
+    return;
+     }
+
+
+   String pin = "" + id.charAt(4) + id.charAt(6);
+
+
+        System.out.println("PIN: " + pin);
     }
-    
-	public static void main(String[] args) {
-	  Scanner sc = new Scanner(System.in);
-	  System.out.println("Enter the message");
-	  String id = sc.nextLine();
-	  
-	  UserInterface obj = new UserInterface();
-	   obj.checkValid(id);
-	  
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the membership ID:");
+        String id = sc.nextLine();
+
+        Main obj = new Main();
+        obj.checkValid(id);
+
+        sc.close();
+    }
 }
