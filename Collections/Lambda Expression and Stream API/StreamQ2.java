fed8e182800911b68e3056393d3ca3115fc0de44
Ram Krishna Pandey <133558506+Ramkrishna8888@users.noreply.github.com>
2025-02-18 00:59:36 +0530

    Create StreamQ2.java

---
+++ Collections/Lambda Expression and Stream API/StreamQ2.java
@@ -0,0 +1,60 @@
+// List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+import java.util.*;
+import java.util.stream.*;
+class GreaterStream{
+    
+    public List<Integer> GreaterThanTen(List<Integer> numbers){
+      return numbers.stream().filter(e->e>10).collect(Collectors.toList());
+    }
+    
+    
+}
+
+class Numbers{
+    private List<Integer> list = new ArrayList<>();
+    
+    Numbers(List<Integer> list){
+        this.list = list;
+    }
+    
+    public List<Integer> getList(){
+        return list;
+    }
+    
+    public void setList (List<Integer> list){
+        this.list = list;
+    }
+    
+    @Override
+    public String toString(){
+        return list.toString();
+    }
+    
+}
+
+class Main{
+    public static void main(String[] args){
+     
+     Scanner sc = new Scanner(System.in);   
+     System.out.println("Enter the size of list");
+      int size = sc.nextInt();
+      List<Integer> list = new ArrayList<>();
+      for (int i =0; i<size; i++){
+          list.add(sc.nextInt());
+      } 
+        System.out.println("Original List");
+        Numbers original = new Numbers(list);
+        System.out.println(original);
+        
+        System.out.println("Numbers Greater than 10");
+        
+        GreaterStream obj = new GreaterStream();
+        List<Integer> NewList = obj.GreaterThanTen(original.getList());
+        
+        System.out.println(NewList);
+        
+        sc.close();
+    }
+    
+}
